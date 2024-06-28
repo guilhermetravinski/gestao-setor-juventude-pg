30e506308d37260a7ae92d885286d9bd326157a9
Guilherme Travinski Ferreira <guitrafer@gmail.com>
2024-06-28 14:47:18 +1300

    feat: add data table

---
+++ src/app/(protected)/grupos/page.tsx
@@ -0,0 +1,111 @@
+import Link from 'next/link'
+
+import { Card, CardContent } from '@/components/ui/card'
+
+import { columns, Payment } from './columns'
+import { DataTable } from './data-table'
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: '728ed52f',
+      amount: 100,
+      status: 'pending',
+      email: 'm@example.com',
+    },
+    {
+      id: '728ed52f',
+      amount: 100,
+      status: 'pending',
+      email: 'm@example.com',
+    },
+    {
+      id: '489e1d42',
+      amount: 125,
+      status: 'processing',
+      email: 'example@gmail.com',
+    },
+    {
+      id: '728ed52f',
+      amount: 100,
+      status: 'pending',
+      email: 'm@example.com',
+    },
+    {
+      id: '489e1d42',
+      amount: 125,
+      status: 'processing',
+      email: 'example@gmail.com',
+    },
+    {
+      id: '728ed52f',
+      amount: 100,
+      status: 'pending',
+      email: 'm@example.com',
+    },
+    {
+      id: '489e1d42',
+      amount: 125,
+      status: 'processing',
+      email: 'example@gmail.com',
+    },
+    {
+      id: '728ed52f',
+      amount: 100,
+      status: 'pending',
+      email: 'm@example.com',
+    },
+    {
+      id: '489e1d42',
+      amount: 125,
+      status: 'processing',
+      email: 'example@gmail.com',
+    },
+    {
+      id: '728ed52f',
+      amount: 100,
+      status: 'pending',
+      email: 'm@example.com',
+    },
+    {
+      id: '489e1d42',
+      amount: 125,
+      status: 'processing',
+      email: 'example@gmail.com',
+    },
+  ]
+}
+
+export default async function GruposPage() {
+  const data = await getData()
+  return (
+    <main className="bg-muted/40 flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10">
+      <div className="mx-auto grid w-full max-w-6xl gap-2">
+        <h1 className="text-3xl font-semibold">Grupos de jovens</h1>
+      </div>
+      <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
+        <nav
+          className="text-muted-foreground grid gap-4 text-sm"
+          x-chunk="dashboard-04-chunk-0"
+        >
+          <Link href="#" className="text-primary font-semibold">
+            General
+          </Link>
+          <Link href="#">Security</Link>
+          <Link href="#">Integrations</Link>
+          <Link href="#">Support</Link>
+          <Link href="#">Organizations</Link>
+          <Link href="#">Advanced</Link>
+        </nav>
+        <div className="grid gap-6">
+          <Card x-chunk="dashboard-04-chunk-1">
+            <CardContent>
+              <DataTable columns={columns} data={data} />
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </main>
+  )
+}
---
+++ src/app/(protected)/nav-link.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import Link from 'next/link'
+import { useSelectedLayoutSegment } from 'next/navigation'
+
+export default function NavLink({
+  slug,
+  children,
+}: {
+  slug: string
+  children: React.ReactNode
+}) {
+  const segment = useSelectedLayoutSegment()
+  const isActive = slug === segment
+
+  return (
+    <Link
+      href={`/${slug}`}
+      className={`${isActive ? 'text-foreground' : 'text-muted-foreground'} hover:text-foreground transition-colors`}
+    >
+      {children}
+    </Link>
+  )
+}
